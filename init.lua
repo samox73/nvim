a3a132e3dd926ec6ad46ccd2564da3f257f74c9a
Samuel Recker <samuel.recker@gmail.com>
2024-10-07 10:21:10 +0200

    remove nvim.notify

--- init.lua
+++ init.lua
@@ -309,9 +309,9 @@ require('lazy').setup({
       -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
       'MunifTanjim/nui.nvim',
       -- OPTIONAL:
-      --   `nvim-notify` is only needed, if you want to use the notification view.
-      --   If not available, we use `mini` as the fallback
-      'rcarriga/nvim-notify',
+      -- `nvim-notify` is only needed, if you want to use the notification view.
+      -- If not available, we use `mini` as the fallback
+      -- 'rcarriga/nvim-notify',
     },
   },
   {
