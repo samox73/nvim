751b61adb4b0d261d88518b39e5a83b2e571d238
Samuel Recker <samuel.recker@gmail.com>
2024-10-31 13:40:31 +0100

    add context

--- lua/custom/plugins/luasnip.lua
+++ lua/custom/plugins/luasnip.lua
@@ -11,20 +11,15 @@ return {
         updateevents = 'TextChanged,TextChangedI',
         enable_autosnippets = true,
       }
-      vim.keymap.set({ 'i', 's' }, '<tab>', function()
-        if ls.expand_or_jumpable() then
-          ls.expand_or_jump()
-        end
+      vim.keymap.set({ 'i' }, '<C-K>', function() ls.expand() end)
+      vim.keymap.set({ 'i', 's' }, '<Tab>', function()
+        if ls.expand_or_jumpable() then ls.expand_or_jump() end
       end, { silent = true })
-      vim.keymap.set({ 'i', 's' }, '<s-tab>', function()
-        if ls.jumpable(-1) then
-          ls.jump(-1)
-        end
+      vim.keymap.set({ 'i', 's' }, '<S-Tab>', function()
+        if ls.jumpable(-1) then ls.jump(-1) end
       end, { silent = true })
       vim.keymap.set({ 'i', 's' }, '<F48>', function()
-        if ls.choice_active() then
-          ls.change_choice(1)
-        end
+        if ls.choice_active() then ls.change_choice(1) end
       end, { silent = true })
       vim.keymap.set('n', '<leader>rs', '<cmd>tabnew ~/.config/nvim/lua/custom/snippets/init.lua<cr>')
       vim.keymap.set('n', '<leader>rr', "lua require 'custom.snippets.lua'")
