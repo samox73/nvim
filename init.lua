ce5b6e4441c6a322f761b8deaf6e2e50add9d27e
Samuel Recker <samuel.recker@gmail.com>
2024-11-21 09:47:46 +0100

    remove luasnip explicit

--- init.lua
+++ init.lua
@@ -736,6 +736,9 @@ require('lazy').setup({
             end,
           },
         },
+        config = function()
+          require 'custom.snippets'
+        end,
       },
       'saadparwaiz1/cmp_luasnip',
 
@@ -776,7 +779,7 @@ require('lazy').setup({
           -- Accept ([y]es) the completion.
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
+          ['<CR>'] = cmp.mapping.confirm { select = true },
 
           -- If you prefer more traditional completion keymaps,
           -- you can uncomment the following lines
