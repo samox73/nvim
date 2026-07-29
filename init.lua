497d987d1a61ec2ac1927ab9ab019fa82d9455ca
Samuel Recker <samuel.recker@gmail.com>
2026-07-29 10:53:51 +0200

    fix: move through wrapped rows with kj

--- init.lua
+++ init.lua
@@ -164,6 +164,10 @@ vim.opt.scrolloff = 10
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+-- Move through wrapped display lines with j/k.
+vim.keymap.set({ 'n', 'x' }, 'j', 'gj')
+vim.keymap.set({ 'n', 'x' }, 'k', 'gk')
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
